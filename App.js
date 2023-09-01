2c846655be8ca8cac7b2559831d0fe8c6e9f81f5
HarshaK64 <129108879+HarshaK64@users.noreply.github.com>
2023-09-01 11:37:13 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import './App.css';
+import RewardPointsCalculator from './components/RewardPointsCalculator';
+
+function App() {
+  return (
+    <div className="App">
+      <RewardPointsCalculator />
+    </div>
+  );
+}
+
+export default App;
